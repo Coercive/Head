9923bec8271c6dee9c93573fff7c41f129db945c
Coercive <contact@coercive.fr>
2019-05-10 15:01:23 +0200

    fix =

--- lib/Title.php
+++ lib/Title.php
@@ -17,7 +17,7 @@ class Title extends GenericAccessors
 	{
 		$html = '<title ';
 		foreach ($this->getArrayCopy() as $attr => $data) {
-			$html .= $attr . '"' . str_replace('"', '', $data) . '" ';
+			$html .= $attr . '="' . str_replace('"', '', $data) . '" ';
 		}
 		$html .= '/>';
 		return $html;
